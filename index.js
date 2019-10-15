7da2f7190134b6b136642a8b87aa2fcbe8569e66
Meresnes <meresnes@yandex.ru>
2019-10-15 14:58:40 +0300

    15.10.2019 14:58

--- index.js
+++
@@ -1,8 +0,0 @@
-/*
-const concat = (...args) => [...args].reduce((r, g, b) => `${r}${g}${b}`);
-module.exports = concat;
-
- console.log(concat(2,6,7)); */
-
- const f = x => x === 1 ? 1 : x * f(x - 1);
- console.log(f(3));
