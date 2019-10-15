b2bcb99d38ba11f99cdd54982cb06aee1439f257
Meresnes <meresnes@yandex.ru>
2019-10-16 02:08:31 +0300

    16.10.2019 02:08

--- Graphic/Labs/Lr_2/Zadanie_2.py
+++ Graphic/Labs/Lr_2/Zadanie_2.py
@@ -1,5 +1,6 @@
 import turtle
 import time
+from tkinter import *
 
 def draw_triangle_1(ttl,color,size,x,y):
 
@@ -33,27 +34,58 @@ def draw_triangle_3(ttl,color,size,x,y):
     ttl.goto(x + size, y)
 
 
-def main():
+
+def drawing(x,y,z):
 
     turtle.tracer(0, 0)
     turtle.hideturtle()
 
     ttl = turtle.Turtle()
     ttl.hideturtle()
-
-    x = int(input("Введите X:"))
-    y = int(input("Введите Y:"))
-
     while True:
-
         time.sleep(0.2)
         ttl.clear()
 
-        draw_triangle_1(ttl,'black',x,x,y)
-        draw_triangle_2(ttl,'red',x,x,y)
-        draw_triangle_3(ttl,'blue',x,x,y)
+        draw_triangle_1(ttl, 'black', z, x, y)
+        draw_triangle_2(ttl, 'red', z, x, y)
+        draw_triangle_3(ttl, 'blue', z, x, y)
 
         turtle.update()
 
-if __name__ == '__main__':
-    main()
+
+def Enter_coords():
+
+    x = EntryX.get()
+    x = float(x)
+
+    y = EntryY.get()
+    y = float(y)
+
+    z = EntryZ.get()
+    z = float(z)
+
+    drawing(x,y,z)
+
+
+root = Tk()
+root.geometry("900x900")
+root.title('Треугольник')
+
+
+Label(root, text='Enter X:').pack()
+EntryX = Entry(root, width=10, font='Arial 16')
+EntryX.pack()
+
+Label(root, text='Enter Y:').pack()
+EntryY = Entry(root, width=10, font='Arial 16')
+EntryY.pack()
+
+Label(root, text='Enter Lenth:').pack()
+EntryZ = Entry(root, width=10, font='Arial 16')
+EntryZ.pack()
+
+but = Button(root, text='Отрисовать')
+but = Button(root, text='Отрисовать',command = Enter_coords)
+but.pack()
+
+root.mainloop()
