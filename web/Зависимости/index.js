1987cfa7f8435a02f8dfd883c7058868ac757c2c
meresnes <meresnes@yandex.ru>
2020-06-25 07:27:54 +0300

    'web'

---
+++ web/Зависимости/index.js
@@ -0,0 +1,17 @@
+const concat = require('goss_concat');
+
+function rgb(r = 255, g = 255, b = 255) {
+  return concat('rgb(', r, ',', g, ',', b, ')');
+}
+
+console.log(rgb(14, 188));
+  
+document.querySelector('button')
+.addEventListener(
+  'click',
+   e => {
+     const result = rgb(255,10);
+     document.body.style.backgroundColor = result;
+     e.target.textContent = result; 
+   }
+);
